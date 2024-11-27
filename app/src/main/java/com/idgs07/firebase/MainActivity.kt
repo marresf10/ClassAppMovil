065c01596caffb41222920575b9c00f398bd20ee
marlene fonseca <fonseca34689@gmail.com>
2024-11-27 00:37:40 -0600

    delete all users finished

--- app/src/main/java/com/idgs07/firebase/MainActivity.kt
+++ app/src/main/java/com/idgs07/firebase/MainActivity.kt
@@ -26,76 +26,51 @@ import com.idgs07.firebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mainBinding : ActivityMainBinding
+    lateinit var mainBinding: ActivityMainBinding
 
-    //Retrieving
-    val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-    val myReference: DatabaseReference = database.reference.child("MyUsers")
-
-    //Retrieving data 2
-    val userList = ArrayList<Users>()
-    lateinit var usersAdapter: UsersAdapter
+    // Firebase
+    private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val myReference: DatabaseReference = database.reference.child("MyUsers")
+    private val userList = ArrayList<Users>()
+    private lateinit var usersAdapter: UsersAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        mainBinding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        val view = mainBinding.root
-        setContentView(view)
-        enableEdgeToEdge()
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
 
-        mainBinding.floatingActionButton.setOnClickListener{
-            val intent  = Intent (this, AddUsersActivity::class.java)
+        //declarar toolbar
+        setSupportActionBar(mainBinding.toolbar)
+
+        mainBinding.floatingActionButton.setOnClickListener {
+            val intent = Intent(this, AddUsersActivity::class.java)
             startActivity(intent)
         }
 
         retrievingDataFromDatabase()
 
-        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
-            0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
-        ) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                TODO("NOT YET IMPLEMENTED")
-            }
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder) = false
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                //viewHolder.adapterPosition
-
                 val id = usersAdapter.getUserId(viewHolder.adapterPosition)
                 myReference.child(id).removeValue()
-                Toast.makeText(applicationContext, "The user was deleted",
-                    Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "The user was deleted", Toast.LENGTH_SHORT).show()
             }
         }).attachToRecyclerView(mainBinding.recyclerView)
-        //Finished
     }
 
-
-    fun retrievingDataFromDatabase() {
+    private fun retrievingDataFromDatabase() {
         myReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 userList.clear()
                 for (eachUser in snapshot.children) {
                     val user = eachUser.getValue(Users::class.java)
-                    if (user != null) {
-                        println("UserId: ${user.userId}")
-                        println("UserId: ${user.userName}")
-                        println("UserId: ${user.userAge}")
-                        println("UserId: ${user.userEmail}")
-                        println("****************************")
-
-                        //Class 2
-                        userList.add(user)
-                    }
-
-                    //Class2
-                    usersAdapter = UsersAdapter(this@MainActivity, userList)
-                    mainBinding.recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
-                    mainBinding.recyclerView.adapter = usersAdapter
+                    if (user != null) userList.add(user)
                 }
+                usersAdapter = UsersAdapter(this@MainActivity, userList)
+                mainBinding.recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                mainBinding.recyclerView.adapter = usersAdapter
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -110,33 +85,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == R.id.deleteAll){
+        if (item.itemId == R.id.deleteAll) {
             showDialogMessage()
         }
         return super.onOptionsItemSelected(item)
     }
 
-    fun showDialogMessage() {
-        val dialogMessage = AlertDialog.Builder(this)
-        dialogMessage.setTitle("Delete all users")
-        dialogMessage.setMessage(
-            "If you click yes, all users will be deleted, " +
-                    "if you want to delete a specific user swipe the item you want to delete"
-        )
-        dialogMessage.setNegativeButton("Cancel") { dialogInterface, i ->
-
-        }
-        dialogMessage.setPositiveButton("Yes") { dialogInterface, i ->
-            myReference.removeValue().addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    usersAdapter.notifyDataSetChanged()
-                    Toast.makeText(applicationContext, "All users deleted successfully", Toast.LENGTH_SHORT).show()
+    private fun showDialogMessage() {
+        AlertDialog.Builder(this)
+            .setTitle("Delete all users")
+            .setMessage("If you click yes, all users will be deleted. If you want to delete a specific user, swipe the item.")
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("Yes") { _, _ ->
+                myReference.removeValue().addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        usersAdapter.notifyDataSetChanged()
+                        Toast.makeText(applicationContext, "All users deleted successfully", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
-        }
-        dialogMessage.create().show()
+            .create()
+            .show()
     }
-
 
 }
 
